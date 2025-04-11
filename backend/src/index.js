b6931a1f6c74860b0f73fbec75d28831e9021b13
SURAJ S <116282102+suraj0-11@users.noreply.github.com>
2025-04-11 23:28:41 +0530

    lol

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,11 @@ const { redirectUrl } = require('./controllers/urlController');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
